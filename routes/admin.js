5c3f2a4d020d67fd9f176a47de4bbf0c4a5af21a
zhaoangus <zhao_angus@163.com>
2019-01-26 11:53:37 +0800

    adminedit v1

--- routes/admin.js
+++ routes/admin.js
@@ -35,6 +35,57 @@ router.post('/user', async ctx => {
   }
 })
 
+router.post('/admin/add', async ctx => {
+  let username = ctx.request.body.username
+  const res = await User.findOneAndUpdate({name: username}, {$set: 
+    {isAdmin: true}
+  })
+  if (res) {
+    ctx.body = {
+      code: 0,
+      res
+    }
+  } else {
+    ctx.body = {
+      code: 1,
+      msg: '出错了！'
+    }
+  }
+})
+
+router.post('/admin/remove', async ctx => {
+  let uid = ctx.request.body.uid
+  const res = await User.findOneAndUpdate({uid}, {$set: 
+    {isAdmin: false}
+  })
+  if (res) {
+    ctx.body = {
+      code: 0,
+      res
+    }
+  } else {
+    ctx.body = {
+      code: 1,
+      msg: '出错了！'
+    }
+  }
+})
+
+router.get('/admin/list', async ctx => {
+  const res = await User.find({isAdmin: true}).sort({uid: 1})
+  if (res) {
+    ctx.body = {
+      code: 0,
+      res
+    }
+  } else {
+    ctx.body = {
+      code: 1,
+      msg: '出错了！'
+    }
+  }
+})
+
 // router.post('/user/:uid', async ctx => {
 //   let uid = parseInt(ctx.params.uid)
 //   let nick = ctx.request.body.nick
